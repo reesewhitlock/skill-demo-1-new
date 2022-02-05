355711e8e9b430c3fc95a98ecee3b3e942dc8cdc
reesewhitlock <rwhitlock@ucsd.edu>
2022-02-04 16:31:50 -0800

    failed test

---
+++ SkillDemoTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void divisionTest() {
+        SkillDemo skillDemo = new SkillDemo();
+        assertEquals(1, skillDemo.division(4, 2));
+    }
+}
